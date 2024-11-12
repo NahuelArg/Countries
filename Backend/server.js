4804c0a4da5916df4a5615d570914eac1ca72b14
NahuelArg <claudioarganaraz86@gmail.com>
2024-11-12 19:58:16 -0300

    fixing code

--- Backend/server.js
+++ Backend/server.js
@@ -8,16 +8,18 @@ const countryRoutes = require('./router/index');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// CORS configuration: Allow frontend (React) on localhost:3000 to access backend
 app.use(cors({
-  origin: 'http://localhost:3000'
+  origin: 'http://localhost:3000',  // Adjust this to the frontend's URL if deployed
 }));
+
+// Middleware to parse incoming JSON requests
 app.use(express.json());
 
-
-// Endpoint to get country info
+// Routes: Handles API endpoints related to country data
 app.use('/api', countryRoutes);
 
+// Start the server and listen on specified port
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
-
